42393850c0817e9c7a8b5c1ea191039f3c785186
togar <mabduhsiregar@gmail.com>
2018-05-21 15:54:13 +0700

    First commit

---
+++ app/src/main/java/com/example/togar/barvolume/MainModel.java
@@ -0,0 +1,13 @@
+package com.example.togar.barvolume;
+
+public class MainModel {
+    private double volume;
+
+    public MainModel(double volume) {
+        this.volume = volume;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+}
---
+++ app/src/main/java/com/example/togar/barvolume/MainView.java
@@ -0,0 +1,5 @@
+package com.example.togar.barvolume;
+
+public interface MainView {
+    void tampilVolume(MainModel model);
+}
